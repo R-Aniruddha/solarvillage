2e9ff2ee912ab0bcf965b035f9a2bb5605d0a0c8
Mansur Rahamathullah <fre2mansur@gmail.com>
2019-02-14 13:33:05 +0530

    created google search page

---
+++ templates/google-search.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+Template Name: bing-search
+*/
+
+if(!get_query_var('q')){
+	wp_redirect(home_url());
+	exit();
+}
+?>
+<?php get_header(); ?>
+    <?php echo  get_query_var('q'); ?>
+    hello
+<?php get_footer(); ?>
